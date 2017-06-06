85488329d7c6df7306cba68bf31488a77a4a4d7d
mr-bel3 <abeltre@skidmore.edu>
2017-06-06 15:18:15 -0400

    6/6
    end of the day... sass colors

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('hello', function(){
+	console.log('Hello World!')
+})
+
+gupl.task('sass', function(){
+	return gulp.src('app/scss/styles.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('app/css'))
+});
+
+

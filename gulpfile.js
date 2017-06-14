55a77645cb9ed1aad5e6a90ca9b38067e70e7b5e
mr-bel3 <abeltre@skidmore.edu>
2017-06-14 15:13:24 -0400

    finished gulp tutorial

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+
 
 gulp.task('hello', function(){
 	console.log('Hello World!')
-})
+});
 
 gulp.task('sass', function(){
 	return gulp.src('app/scss/**/*.scss')
@@ -16,8 +26,10 @@ gulp.task('sass', function(){
 });
 
 gulp.task('watch', ['browserSync', 'sass'], function(){
-	gulp.watch('app/scss/**/*.scss', ['sass', ]);
-})
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reaload);
+});
 
 gulp.task('browserSync', function(){
 	browserSync.init({
@@ -25,4 +37,38 @@ gulp.task('browserSync', function(){
 			baseDir: 'app'
 		},
 	})
+});
+
+gulp.task('useref', function(){
+	return gulp.src('app/*.html')
+	.pipe(useref())
+	.pipe(gulpIf('*.js', uglify()))
+	.pipe(gulpIf('*.css', cssnano()))
+	.pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+	return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+	.pipe(cache(imagemin({
+		interlaced : true
+	})))
+	.pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function(){
+	return gulp.src('app/fonts/**/*')
+	.pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('clean:dist', function(){
+	return del.sync('dist');
 })
+
+gulp.task('cache:clear',function(callback){
+	return cache.clearAll(callback)
+});
+
+gulp.task('default', function(callback){
+	runSequence('clean:dist', ['sass', 'useref', 'images', 'fonts'], callback)
+});
+
